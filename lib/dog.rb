051b2f5d47d4207802ba9258024a6470bd7c116e
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-02-22 03:50:13 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,16 +4,10 @@ class Dog
      @name = name 
       end 
 	
-	def name 
-	    @name 
-	  end 
-	  
-	  def initialize(breed)
-	    @breed = breed
-	  end 
-	  
-	  def breed 
-	    @breed 
-	  end 
+	
+	
+	
+	
+	
 	
 	end 
