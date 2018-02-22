0a00a3c18d6f6aaf0f0d73ba665edbf578f0d1fc
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-02-22 03:53:31 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,13 @@
 class Dog 
   
-   def initialize(dog_name, dog_breed) 
+   def initialize(dog_name, dog_breed = "Mutt") 
      @dog_name = dog_name 
      @dog_breed = dog_name 
-     
-	
-	
-	
-	
-	
-	
-	
-	end 
+   end 
+   
+   def pet_namet 
+     @dog_name
+	   @dog_breed
+	   
+	 end 
+		end 
